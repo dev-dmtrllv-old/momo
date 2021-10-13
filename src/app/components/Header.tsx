16b33b0e72fd68eec5ecc4f0df6eba6736e06d67
dmtrllv <dev.dmtrllv@gmail.com>
2021-10-13 22:39:51 +0200

    updated layout a bit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 import { SectionStore } from "app/stores/SectionStore";
 import { Store } from "app/stores/Store";
-import { FlexBox, FlexItem, View } from "app/views";
+import { Container, FlexBox, FlexItem, View } from "app/views";
 import React from "react";
 
 import "./styles/header.scss";
@@ -9,26 +9,28 @@ export const Header: React.FC = Store.withStore(SectionStore, ({ store }) =>
 {
 	return (
 		<View fill id="header">
-			<View className="wrapper" position="absolute">
-				<FlexBox>
-					<FlexItem base={200}>
-						<h1>{store.title}</h1>
-					</FlexItem>
-					<FlexItem>
-						<View className="links" position="absolute">
-							{SectionStore.titles.map((title, i) => 
-							{
-								const isActive = title === store.title;
-								return (
-									<a key={i} className={isActive ? "active" : ""} onClick={() => store.routeTo(title)}>
-										{title
-										}</a>
-								);
-							})}
-						</View>
-					</FlexItem>
-				</FlexBox>
-			</View>
+			<Container fill >
+				<View className="wrapper" position="absolute">
+					<FlexBox>
+						<FlexItem base={200}>
+							<h1>{store.title}</h1>
+						</FlexItem>
+						<FlexItem>
+							<View className="links" position="absolute">
+								{SectionStore.titles.map((title, i) => 
+								{
+									const isActive = title === store.title;
+									return (
+										<a key={i} className={isActive ? "active" : ""} onClick={() => store.routeTo(title)}>
+											{title}
+										</a>
+									);
+								})}
+							</View>
+						</FlexItem>
+					</FlexBox>
+				</View>
+			</Container>
 		</View>
 	);
 });
