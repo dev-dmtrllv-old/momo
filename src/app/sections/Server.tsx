161ad70951e6f80b9c0e9d1b91991f9239436896
dmtrllv <dev.dmtrllv@gmail.com>
2021-10-14 18:42:49 +0200

    redirect to home on delete

--- src/app/sections/Server.tsx
+++ src/app/sections/Server.tsx
@@ -2,10 +2,11 @@ import { ServersStore } from "app/stores/ServersStore";
 import { Store } from "app/stores/Store";
 import { Button, Container } from "app/views";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useLocation, useParams } from "react-router-dom";
 
 export const Server = Store.withStore(ServersStore, ({ store }) =>
 {
+	const { replace } = useHistory();
 	const { name } = useParams<{ name: string }>();
 
 	const s = store.get("servers").find(s => s.name === name);
@@ -15,7 +16,7 @@ export const Server = Store.withStore(ServersStore, ({ store }) =>
 		<Container>
 			<h1>hi server {name}!</h1>
 			<p>version: {v}</p>
-			<Button onClick={() => store.delete(name)}>delete</Button>
+			<Button onClick={() => { replace("/"); store.delete(name); }}>delete</Button>
 			<Button onClick={() => store[store.runningServers[name]?.isRunning ? "stop" : "start"](name)}>{store.runningServers[name]?.isRunning ? "stop" : "start"}</Button>
 			<h1>
 				Data
